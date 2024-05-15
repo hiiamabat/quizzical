51f6575a92e694d92f4af5c0ca70976de7ec5760
hiiamabat <hiiamabat@gmail.com>
2024-05-15 15:30:51 -0600

    Second initial upload after first for troubleshooting
    The first upload was very basic/small but I was having trouble with strictmode causing the 429 errors from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Start from "./components/Start/Start";
 import Quiz from "./components/Quiz/Quiz";
 import "./App.css";
+import he from "he";
 
 function App() {
   const [quizStarted, setQuizStarted] = useState(false);
+  const [quizData, setQuizData] = useState(null);
+
+  const getQuizData = async () => {
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&difficulty=easy"
+      );
+      const data = await response.json();
+      const questions = data.results.map((question, index) => {
+        let answers;
+        if (question.type === "boolean") {
+          answers = [
+            he.decode(question.correct_answer),
+            he.decode(question.incorrect_answers[0]),
+          ];
+        } else {
+          answers = [
+            he.decode(question.correct_answer),
+            ...question.incorrect_answers.map(he.decode),
+          ].sort(() => Math.random() - 0.5);
+        }
+        return {
+          id: index++,
+          question: he.decode(question.question),
+          correct_answer: he.decode(question.correct_answer),
+          incorrect_answers: question.incorrect_answers.map(he.decode),
+          answers,
+          selectedAnswer: "",
+        };
+      });
+      setQuizData(questions);
+    } catch (error) {
+      console.error("Error fetching quiz data:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Make sure to not fetch if the data is already there
+    if (!quizData) {
+      getQuizData();
+    }
+  }, [quizData]);
 
   function beginPlay() {
     setQuizStarted(true);
   }
 
-  return quizStarted ? <Quiz /> : <Start beginPlay={beginPlay} />;
+  return quizStarted ? (
+    <Quiz quizData={quizData} getNewQuizData={getQuizData} />
+  ) : (
+    <Start beginPlay={beginPlay} />
+  );
 }
 
 export default App;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,15 +1,120 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Quiz.css";
 
-export default function Quiz() {
-  React.useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
+export default function Quiz({ quizData, getNewQuizData }) {
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [showResults, setShowResults] = useState(false);
+  const [newGame, setNewGame] = useState(false);
+  const [currentQuizData, setCurrentQuizData] = useState([]);
+
+  useEffect(() => {
+    setCurrentQuizData(quizData);
+  }, [quizData]);
+
+  const handlePlayNewGame = () => {
+    getNewQuizData();
+    setShowResults(false);
+    setSelectedAnswers({});
+  };
+
+  const handleAnswerSelect = (questionId, answer) => {
+    setSelectedAnswers({ ...selectedAnswers, [questionId]: answer });
+  };
+
+  const checkAnswers = () => {
+    setShowResults(true);
+  };
+
+  const countCorrectAnswers = () => {
+    let correctCount = 0;
+    currentQuizData.forEach((question) => {
+      if (selectedAnswers[question.id] === question.correct_answer) {
+        correctCount++;
+      }
+    });
+    return correctCount;
+  };
+
   return (
     <div className="quiz">
-      <h1>Quiz</h1>
+      <form>
+        {currentQuizData &&
+          currentQuizData.map((question, index) => (
+            <div key={index}>
+              <fieldset>
+                <legend>
+                  <h2>{question.question}</h2>
+                </legend>
+                <div className="answers">
+                  {question.answers.map((answer, ansIndex) => {
+                    const isSelected = selectedAnswers[question.id] === answer;
+                    const isCorrect = answer === question.correct_answer;
+                    let backgroundColor = "";
+                    let opacity = 1;
+                    let color = "";
+                    let border = "";
+
+                    if (showResults) {
+                      if (isCorrect) {
+                        backgroundColor = "#94D7A2";
+                        border = "none";
+                      } else if (isSelected && !isCorrect) {
+                        backgroundColor = "#F8BCBC";
+                        border = "none";
+                      } else {
+                        console.log("Setting opacity to 0.5");
+                        opacity = 0.5;
+                      }
+                    }
+
+                    return (
+                      <label
+                        key={ansIndex}
+                        htmlFor={`${question.id}_${answer}`}
+                        className="radio-button"
+                        style={{
+                          backgroundColor,
+                          opacity,
+                          color,
+                          border,
+                          pointerEvents: showResults ? "none" : "auto",
+                        }}
+                      >
+                        <input
+                          type="radio"
+                          id={`${question.id}_${answer}`}
+                          name={answer}
+                          value={answer}
+                          checked={isSelected}
+                          onChange={() =>
+                            handleAnswerSelect(question.id, answer)
+                          }
+                        />
+                        {answer}
+                      </label>
+                    );
+                  })}
+                </div>
+              </fieldset>
+            </div>
+          ))}
+      </form>
+      {!showResults && (
+        <button className="btn" onClick={checkAnswers}>
+          Check Answers
+        </button>
+      )}
+      {showResults && (
+        <div className="results">
+          <button className="btn" onClick={handlePlayNewGame}>
+            Play New Game
+          </button>
+          <p>
+            You got {countCorrectAnswers()} out of {currentQuizData.length}{" "}
+            right
+          </p>
+        </div>
+      )}
     </div>
   );
 }
